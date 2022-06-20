0cb1d723713ed86f67775aab75a98af0ec20b7b7
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-06-20 15:50:45 +0200

    dev: make an iife

--- main.js
+++ main.js
@@ -1,26 +1,30 @@
 console.log("start");
 
-var svgns = "http://www.w3.org/2000/svg";
+(function () {
+  var svgns = "http://www.w3.org/2000/svg";
 
-var sampleNbr = 10;
+  var sampleNbr = 10;
 
-var cx0 = 50;
-var cy0 = 50;
-var r0 = 45;
+  var cx0 = 50;
+  var cy0 = 50;
+  var r0 = 45;
 
-var gPoints = document.querySelector("g.points");
-console.log("gPoints: ", gPoints);
-for (var i = 0; i < sampleNbr; i++) {
-  console.log("i: ", i);
+  var gPoints = document.querySelector("g.points");
+  console.log("gPoints: ", gPoints);
+  for (var i = 0; i < sampleNbr; i++) {
+    console.log("i: ", i);
 
-  var angle = (i * (Math.PI * 2)) / sampleNbr;
-  var cx = cx0 + r0 * Math.cos(angle);
-  var cy = cy0 + r0 * Math.sin(angle);
+    var angle = (i * (Math.PI * 2)) / sampleNbr;
+    var cx = cx0 + r0 * Math.cos(angle);
+    var cy = cy0 + r0 * Math.sin(angle);
 
-  var circle = document.createElementNS(svgns, "circle");
-  circle.setAttributeNS(null, "cx", cx);
-  circle.setAttributeNS(null, "cy", cy);
-  circle.setAttributeNS(null, "r", 1);
+    var circle = document.createElementNS(svgns, "circle");
+    circle.setAttributeNS(null, "cx", cx);
+    circle.setAttributeNS(null, "cy", cy);
+    circle.setAttributeNS(null, "r", 1);
 
-  gPoints.appendChild(circle);
-}
+    gPoints.appendChild(circle);
+  }
+})();
+
+console.log("this.cx: ", this.cx);
