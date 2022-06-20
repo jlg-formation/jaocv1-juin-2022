b163ccaf1f4950efb6946f8feb86ea4b9845a0b9
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-06-20 15:38:14 +0200

    dev: added 10 small circles around the big circle

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
 console.log("start");
+
+var svgns = "http://www.w3.org/2000/svg";
+
+var sampleNbr = 10;
+
+var cx0 = 50;
+var cy0 = 50;
+var r0 = 45;
+
+var gPoints = document.querySelector("g.points");
+console.log("gPoints: ", gPoints);
+for (var i = 0; i < sampleNbr; i++) {
+  console.log("i: ", i);
+
+  var angle = (i * (Math.PI * 2)) / sampleNbr;
+  var cx = cx0 + r0 * Math.cos(angle);
+  var cy = cy0 + r0 * Math.sin(angle);
+
+  var circle = document.createElementNS(svgns, "circle");
+  circle.setAttributeNS(null, "cx", cx);
+  circle.setAttributeNS(null, "cy", cy);
+  circle.setAttributeNS(null, "r", 1);
+
+  gPoints.appendChild(circle);
+}
