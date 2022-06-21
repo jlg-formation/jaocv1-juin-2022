b7ddb7bebeb6d525c97d63f8a92fd6afbe80d222
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-06-21 10:27:33 +0200

    dev: board is now es6

--- Board.js
+++ Board.js
@@ -1,41 +1,37 @@
-(function () {
-  "use strict";
-
-  function Board() {
+class Board {
+  constructor() {
     this.config = {
       sampleNbr: 20,
       multiplicationFactor: 3,
     };
-
-    this.setConfig = function (configObject) {
-      this.config = configObject;
-    };
-
-    this.draw = function () {
-      this.drawPoints();
-      this.drawLines();
-    };
-
-    this.drawPoints = function () {
-      for (var i = 0; i < this.config.sampleNbr; i++) {
-        console.log("i: ", i);
-        Point.getFromIndex(i, this.config.sampleNbr).draw();
-      }
-    };
-
-    this.drawLines = function () {
-      for (var i = 0; i < this.config.sampleNbr; i++) {
-        var line = Line.getFromIndexes(
-          i,
-          i * this.config.multiplicationFactor,
-          this.config.sampleNbr
-        );
-
-        console.log("line: ", line);
-        line.draw();
-      }
-    };
   }
 
-  window.Board = Board;
-})();
+  setConfig(configObject) {
+    this.config = configObject;
+  }
+
+  drawPoints() {
+    for (var i = 0; i < this.config.sampleNbr; i++) {
+      console.log("i: ", i);
+      Point.getFromIndex(i, this.config.sampleNbr).draw();
+    }
+  }
+
+  drawLines() {
+    for (var i = 0; i < this.config.sampleNbr; i++) {
+      var line = Line.getFromIndexes(
+        i,
+        i * this.config.multiplicationFactor,
+        this.config.sampleNbr
+      );
+
+      console.log("line: ", line);
+      line.draw();
+    }
+  }
+
+  draw() {
+    this.drawPoints();
+    this.drawLines();
+  }
+}
