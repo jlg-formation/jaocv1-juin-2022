c0abc2f74fe4bfc2fe0724e2dd1045bc19adf057
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-06-22 14:41:43 +0200

    dev: added webpack for development and production

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,32 @@
 const path = require("path");
 
-module.exports = {
-  entry: "./src/main.ts",
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        use: "ts-loader",
-        exclude: /node_modules/,
-      },
-    ],
-  },
-  resolve: {
-    extensions: [".ts", ".js"],
-  },
-  mode: "production",
+module.exports = (_, env) => {
+  const config = {
+    entry: "./src/main.ts",
+    output: {
+      filename: "bundle.js",
+      path: path.resolve(__dirname, "dist"),
+      clean: true,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.ts$/,
+          use: "ts-loader",
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    resolve: {
+      extensions: [".ts", ".js"],
+    },
+
+    mode: env.mode ? env.mode : "production",
+  };
+
+  if (env.mode === "development") {
+    config.devtool = "inline-source-map";
+  }
+
+  return config;
 };
